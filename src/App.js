f2701cd2d99f017d890371059ae544f689b58d56
Hamal Samuel <zariktechnologies@gmail.com>
2022-10-18 08:35:00 -0400

    Updated jsx

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
 import Treats from "./components/Treats";
-import "./App.css";
+import "./index.css";
 
 function App() {
   return (
--- src/components/Treats.jsx
+++ src/components/Treats.jsx
@@ -1,9 +1,45 @@
 import React from "react";
+import supabase from "../config/supabaseClient";
+import { useEffect, useState } from "react";
 
 const Treats = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [treats, setTreats] = useState(null);
+
+  useEffect(() => {
+    const fetchTreats = async () => {
+      const { data, error } = await supabase.from("seinfeldfoods").select(`
+        food_name,
+        episode,
+        episode_name
+      `);
+
+      if (error) {
+        setFetchError("Could not get Treats");
+        setTreats(null);
+        console.log(error);
+      }
+      if (data) {
+        setTreats(data);
+        setFetchError(null);
+        console.log(data);
+      }
+    };
+
+    fetchTreats();
+  }, []);
+
   return (
     <div>
       <h1 className="treats-header">Treats</h1>
+      {fetchError && <p>{fetchError}</p>}
+      {treats && (
+        <div className="treats">
+          {treats.map((treats) => (
+            <p>{treats.title}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
